a334eb7d7be9c7319043a3521f65699cd8dd6d2e
Phong <tranloiphong2@gmail.com>
2017-07-01 18:10:00 +0700

    Working react-md - Able to open drawer - Install webpack-copy-plugin dep

--- app/component/UserDrawer.jsx
+++ app/component/UserDrawer.jsx
@@ -1,30 +1,62 @@
-import React,{PureComponent} from "react"
+import React, {PureComponent} from "react"
 import Drawer from 'react-md/lib/Drawers';
 import Button from 'react-md/lib/Buttons/Button';
 import Toolbar from 'react-md/lib/Toolbars';
-import inboxListItems from 'constants/inboxListItems';
+
+// import inboxListItems from 'constants/inboxListItems';
 
 class UserDrawer extends PureComponent {
-    constructor(){
+    constructor() {
         super();
+
+        this.state = {
+            visible: false,
+            position: 'left',
+        };
+
+        this._toggleLeft = this._toggleLeft.bind(this);
+        this._toggleRight = this._toggleRight.bind(this);
+        this._closeDrawer = this._closeDrawer.bind(this);
+        this._handleToggle = this._handleToggle.bind(this);
     }
 
-    render(){
-        let {
+    _closeDrawer() {
+        this.setState({ visible: false });
+    }
+
+    _handleToggle(visible) {
+        this.setState({visible});
+    }
+
+    _toggleLeft() {
+        this.setState({visible: !this.state.visible, position: 'left'});
+    }
+
+    render() {
+        const {
             children
         } = this.props;
+        const left = this.state.position === 'left';
+        const close = <Button icon onClick={this._closeDrawer}>{left ? 'arrow_back' : 'close'}</Button>;
+        const header = (
+            <Toolbar
+                nav={left ? null : close}
+                actions={left ? close : null}
+                className="md-divider-border md-divider-border--bottom"
+            />
+        );
 
         return (
             <div className="md-grid">
-                <Button raised label="Toggle Drawer Left" onClick={this._toggleLeft} />
-                <Button raised label="Toggle Drawer Right" onClick={this._toggleRight} />
+                <Button raised label="Toggle Drawer Left" onClick={this._toggleLeft}/>
+                <Button raised label="Toggle Drawer Right" onClick={this._toggleRight}/>
                 <Drawer
                     {...this.state}
-                    navItems={inboxListItems}
+                    // navItems={inboxListItems}
                     onVisibilityToggle={this._handleToggle}
                     type={Drawer.DrawerTypes.TEMPORARY}
                     header={header}
-                    style={{ zIndex: 100 }}
+                    style={{zIndex: 100}}
                 />
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var webpack = require('webpack');
+require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'express/view');
@@ -10,15 +11,24 @@ var config = {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
-    module : {
-        loaders : [
+    module: {
+        loaders: [
             {
-                test : /\.jsx?/,
-                include : APP_DIR,
-                loader : 'babel-loader'
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel-loader'
             }
         ]
-    }
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: path.join("node_modules", "react-md", "dist", "react-md.indigo-purple.min.css"),
+                to: BUILD_DIR
+            }
+        ])
+    ]
 };
 
 module.exports = config;
